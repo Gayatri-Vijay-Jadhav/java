99399a972f370f4962235d53709628866089b1a8
Gayatri6705 <gvj6705@gmail.com>
2025-07-07 23:03:23 +0530

    Add files via upload

---
+++ Multipication.java
@@ -0,0 +1,11 @@
+public class Multipication {
+    public static void main (String[] arg) 
+        {
+        int a = 5;
+        int b = 10;
+        int c = a * b;
+        System.out.println("The result of multiplication is: " + c);
+
+    }
+}
+
---
+++ addition.java
@@ -0,0 +1,19 @@
+public class addition {
+    public static void main(String[] arg)
+    {
+    int a=10;
+    int b=10;
+    int c=a+b;
+    System.out.println("Addition of two numbers: "+ c);
+
+
+    }
+}
+// public class Addition {
+//     public static void main(String[] args) {
+//         int a = 10;
+//         int b = 10;
+//         int c = a + b;
+//         System.out.println("Addition of two numbers: " + c);
+//     }
+// }
