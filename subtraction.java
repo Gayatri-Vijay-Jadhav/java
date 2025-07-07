8d1ee4a1f4568a6036047479c6d9698eeb336976
Gayatri6705 <gvj6705@gmail.com>
2025-07-07 23:01:09 +0530

    Add files via upload

---
+++ subtraction.java
@@ -0,0 +1,8 @@
+public class subtraction {
+    public static void main (String[] arg){
+        int a =13;
+        int b = 5;
+        int c = a - b;
+        System.out.println("Subtraction of two numbers: " + c);
+        }
+}
