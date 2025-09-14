213406937e268abba9645e7e78523178b87e1d3c
Gayatri Vijay Jadhav <gvj6705@gmail.com>
2025-09-14 21:25:03 +0530

    Method2.java

---
+++ Method2.java
@@ -0,0 +1,13 @@
+public class Method1 {
+    public static void main(String[] arg){
+       greet("Gayatri");
+       sum(10,12);
+    }
+    public static void greet(String name) {
+        System.out.println("Hello My Name Is: "+name);
+        
+    }
+     public static void sum(int a, int b) {
+        System.out.println(a+b);
+}
+}
