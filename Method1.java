9523b00985885e28702d738d878092d707d2438e
Gayatri Vijay Jadhav <gvj6705@gmail.com>
2025-09-14 21:11:57 +0530

    Method1.java

---
+++ Method1.java
@@ -0,0 +1,9 @@
+public class Method1 {
+    public static void main(String[] arg){
+       greet("Gayatri");
+    }
+    public static void greet(String name) {
+        System.out.print("Hello My Name Is: "+name);
+        
+    }
+}
