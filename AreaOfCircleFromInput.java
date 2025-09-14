57de5c087fe6c61d8394abfec419ff1aea4ef8ad
Gayatri Vijay Jadhav <gvj6705@gmail.com>
2025-09-14 20:18:42 +0530

    Create AreaOfCircleFromInput.java

---
+++ AreaOfCircleFromInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Method1 {
+    public static void main(String[] arg){
+    Scanner sc =new Scanner(System.in);
+    System.out.print("Enter a Number: ");
+    int r =sc.nextInt();
+    // Double pi= Math.PI;
+    System.out.println(Math.PI * r * r);
+
+    }
+}
